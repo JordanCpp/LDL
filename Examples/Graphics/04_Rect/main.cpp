fffc72e75f00030e332e77abcafbdbe7c57b24e2
Евгений <jordan631@yandex.ru>
2022-10-26 17:06:35 +0400

    Add example 04_Rect

---
+++ Examples/Graphics/04_Rect/main.cpp
@@ -0,0 +1,37 @@
+#include <LDL/Graphics/Render.hpp>
+#include <LDL/Core/RuntimeError.hpp>
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		LDL::Graphics::Window window(LDL::Graphics::Point2u(0, 0), LDL::Graphics::Point2u(800, 600), "Window!");
+
+		LDL::Graphics::Render render(&window);
+
+		LDL::Events::Event report;
+
+		while (window.GetEvent(report))
+		{
+			render.Color(LDL::Graphics::Color(0, 162, 232));
+			render.Clear();
+
+			if (report.Type == LDL::Events::IsQuit)
+			{
+				window.StopEvent();
+			}
+
+			render.Color(LDL::Graphics::Color(237, 28, 36));
+			render.Fill(LDL::Graphics::Point2u(0, 0), LDL::Graphics::Point2u(400, 300));
+
+			render.Present();
+		}
+	}
+	catch (const LDL::Core::RuntimeError& error)
+	{
+		std::cout << error.what() << '\n';
+	}
+
+	return 0;
+}
