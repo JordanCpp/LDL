a311a5698697c7b19510fd4f8e7c1bfb1aa1d6ca
Евгений <jordan631@yandex.ru>
2022-10-30 18:26:05 +0400

    Add tests for x86 and x64 files

--- Tests_x64.bat
+++ Tests_x64.bat
@@ -1,9 +1,4 @@
 
-for %%f in (out\build\x86-Debug\test\Graphics\*.exe) do call %%f
-for %%f in (out\build\x86-Debug\test\Events\*.exe) do call %%f
-for %%f in (out\build\x86-Debug\test\Allocators\*.exe) do call %%f
-for %%f in (out\build\x86-Debug\test\Loaders\*.exe) do call %%f
-
 for %%f in (out\build\x64-Debug\test\Graphics\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\test\Events\*.exe) do call %%f
 for %%f in (out\build\x64-Debug\test\Allocators\*.exe) do call %%f
---
+++ Tests_x86.bat
@@ -0,0 +1,7 @@
+
+for %%f in (out\build\x86-Debug\test\Graphics\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\test\Events\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\test\Allocators\*.exe) do call %%f
+for %%f in (out\build\x86-Debug\test\Loaders\*.exe) do call %%f
+
+pause
