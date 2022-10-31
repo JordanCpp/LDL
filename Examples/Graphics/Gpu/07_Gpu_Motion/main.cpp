0f7e6e3070935508a66ba40e8fefe690d07ae15d
Евгений <jordan631@yandex.ru>
2022-10-31 10:49:43 +0400

    Add examples for Gpu

---
+++ Examples/Graphics/Gpu/07_Gpu_Motion/main.cpp
@@ -0,0 +1,57 @@
+#include <LDL/Graphics/Gpu/GpuWindow.hpp>
+#include <LDL/Graphics/Gpu/GpuImage.hpp>
+#include <LDL/Graphics/Gpu/GpuRender.hpp>
+#include <LDL/Core/RuntimeError.hpp>
+#include <LDL/Loaders/ImageBufferLoader.hpp>
+#include <LDL/Allocators/FixedLinear.hpp>
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		LDL::Graphics::GpuWindow window(LDL::Graphics::Point2u(0, 0), LDL::Graphics::Point2u(800, 600), "Window!");
+
+		LDL::Graphics::GpuRender render(&window);
+
+		LDL::Events::Event report;
+
+		LDL::Allocators::FixedLinear allocator(LDL::Allocators::Allocator::Mb * 4);
+		LDL::Loaders::ImageBufferLoader loader(&allocator);
+
+		loader.Load("Gorgosaurus_BW_transparent.png");
+		LDL::Graphics::GpuImage image(loader.Size(), loader.BytesPerPixel(), loader.Pixels());
+
+		size_t x = 0;
+		size_t y = 0;
+
+		while (window.GetEvent(report))
+		{
+			render.Begin();
+
+			render.Color(LDL::Graphics::Color(0, 162, 232));
+			render.Clear();
+
+			if (report.Type == LDL::Events::IsQuit)
+			{
+				window.StopEvent();
+			}
+
+			if (report.Type == LDL::Events::IsMouseMove)
+			{
+				x = report.Mouse.PosX;
+				y = report.Mouse.PosY;
+			}
+
+			render.Draw(&image, LDL::Graphics::Point2u(x, y), LDL::Graphics::Point2u(150, 150));
+
+			render.End();
+		}
+	}
+	catch (const LDL::Core::RuntimeError& error)
+	{
+		std::cout << error.what() << '\n';
+	}
+
+	return 0;
+}
--- Win9x/Examples.bat
+++ Win9x/Examples.bat
@@ -4,5 +4,7 @@ call Win9x_build.bat 02_Gpu_Color.exe           Examples\Graphics\Gpu\02_Gpu_Col
 call Win9x_build.bat 03_Gpu_Line.exe            Examples\Graphics\Gpu\03_Gpu_Line\main.cpp
 call Win9x_build.bat 04_Gpu_Rect.exe            Examples\Graphics\Gpu\04_Gpu_Rect\main.cpp
 call Win9x_build.bat 05_Gpu_Image.exe           Examples\Graphics\Gpu\05_Gpu_Image\main.cpp
+call Win9x_build.bat 06_Gpu_Click.exe           Examples\Graphics\Gpu\06_Gpu_Click\main.cpp
+call Win9x_build.bat 07_Gpu_Motion.exe          Examples\Graphics\Gpu\07_Gpu_Motion\main.cpp
 
 pause
