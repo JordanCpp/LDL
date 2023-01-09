e271ce6f48decc9a873c27c145479917db36dccb
Евгений <jordan631@yandex.ru>
2023-01-09 15:41:24 +0400

    Add full support keubord keys

--- source/Platforms/Windows/Graphics/MainWindow.cpp
+++ source/Platforms/Windows/Graphics/MainWindow.cpp
@@ -8,120 +8,106 @@ static const char AppName[] = "MainWindow";
 
 size_t MainWindow::ConvertKey(size_t key)
 {
-    size_t result = 0;
-
     switch (key)
     {
-    case VK_ESCAPE:
-        result = LDL::Enums::KeyboardKey::Escape;
-        break;
-
-    case 'A':
-        result = LDL::Enums::KeyboardKey::A;
-        break;
-
-    case 'B':
-        result = LDL::Enums::KeyboardKey::B;
-        break;
-
-    case'C':
-        result = LDL::Enums::KeyboardKey::C;
-        break;
-
-    case 'D':
-        result = LDL::Enums::KeyboardKey::D;
-        break;
-
-    case 'E':
-        result = LDL::Enums::KeyboardKey::E;
-        break;
-
-    case 'F':
-        result = LDL::Enums::KeyboardKey::F;
-        break;
-
-    case 'G':
-        result = LDL::Enums::KeyboardKey::G;
-        break;
-
-    case 'H':
-        result = LDL::Enums::KeyboardKey::H;
-        break;
-
-    case 'I':
-        result = LDL::Enums::KeyboardKey::I;
-        break;
-
-    case 'J':
-        result = LDL::Enums::KeyboardKey::J;
-        break;
-
-    case 'K':
-        result = LDL::Enums::KeyboardKey::K;
-        break;
-
-    case 'L':
-        result = LDL::Enums::KeyboardKey::L;
-        break;
-
-    case 'M':
-        result = LDL::Enums::KeyboardKey::M;
-        break;
-
-    case 'N':
-        result = LDL::Enums::KeyboardKey::N;
-        break;
-
-    case 'O':
-        result = LDL::Enums::KeyboardKey::O;
-        break;
-
-    case 'P':
-        result = LDL::Enums::KeyboardKey::P;
-        break;
-
-    case 'Q':
-        result = LDL::Enums::KeyboardKey::Q;
-        break;
-
-    case 'R':
-        result = LDL::Enums::KeyboardKey::R;
-        break;
-
-    case 'S':
-        result = LDL::Enums::KeyboardKey::S;
-        break;
-
-    case 'T':
-        result = LDL::Enums::KeyboardKey::T;
-        break;
-
-    case 'U':
-        result = LDL::Enums::KeyboardKey::U;
-        break;
-
-    case 'V':
-        result = LDL::Enums::KeyboardKey::V;
-        break;
-
-    case 'W':
-        result = LDL::Enums::KeyboardKey::W;
-        break;
-
-    case 'X':
-        result = LDL::Enums::KeyboardKey::X;
-        break;
-
-    case 'Y':
-        result = LDL::Enums::KeyboardKey::Y;
-        break;
-
-    case 'Z':
-        result = LDL::Enums::KeyboardKey::Z;
-        break;
+    case VK_LWIN:       return LDL::Enums::KeyboardKey::LSystem;
+    case VK_RWIN:       return LDL::Enums::KeyboardKey::RSystem;
+    case VK_APPS:       return LDL::Enums::KeyboardKey::Menu;
+    case VK_OEM_1:      return LDL::Enums::KeyboardKey::Semicolon;
+    case VK_OEM_2:      return LDL::Enums::KeyboardKey::Slash;
+    case VK_OEM_PLUS:   return LDL::Enums::KeyboardKey::Equal;
+    case VK_OEM_MINUS:  return LDL::Enums::KeyboardKey::Hyphen;
+    case VK_OEM_4:      return LDL::Enums::KeyboardKey::LBracket;
+    case VK_OEM_6:      return LDL::Enums::KeyboardKey::RBracket;
+    case VK_OEM_COMMA:  return LDL::Enums::KeyboardKey::Comma;
+    case VK_OEM_PERIOD: return LDL::Enums::KeyboardKey::Period;
+    case VK_OEM_7:      return LDL::Enums::KeyboardKey::Quote;
+    case VK_OEM_5:      return LDL::Enums::KeyboardKey::Backslash;
+    case VK_OEM_3:      return LDL::Enums::KeyboardKey::Tilde;
+    case VK_ESCAPE:     return LDL::Enums::KeyboardKey::Escape;
+    case VK_SPACE:      return LDL::Enums::KeyboardKey::Space;
+    case VK_RETURN:     return LDL::Enums::KeyboardKey::Enter;
+    case VK_BACK:       return LDL::Enums::KeyboardKey::Backspace;
+    case VK_TAB:        return LDL::Enums::KeyboardKey::Tab;
+    case VK_PRIOR:      return LDL::Enums::KeyboardKey::PageUp;
+    case VK_NEXT:       return LDL::Enums::KeyboardKey::PageDown;
+    case VK_END:        return LDL::Enums::KeyboardKey::End;
+    case VK_HOME:       return LDL::Enums::KeyboardKey::Home;
+    case VK_INSERT:     return LDL::Enums::KeyboardKey::Insert;
+    case VK_DELETE:     return LDL::Enums::KeyboardKey::Delete;
+    case VK_ADD:        return LDL::Enums::KeyboardKey::Add;
+    case VK_SUBTRACT:   return LDL::Enums::KeyboardKey::Subtract;
+    case VK_MULTIPLY:   return LDL::Enums::KeyboardKey::Multiply;
+    case VK_DIVIDE:     return LDL::Enums::KeyboardKey::Divide;
+    case VK_PAUSE:      return LDL::Enums::KeyboardKey::Pause;
+    case VK_F1:         return LDL::Enums::KeyboardKey::F1;
+    case VK_F2:         return LDL::Enums::KeyboardKey::F2;
+    case VK_F3:         return LDL::Enums::KeyboardKey::F3;
+    case VK_F4:         return LDL::Enums::KeyboardKey::F4;
+    case VK_F5:         return LDL::Enums::KeyboardKey::F5;
+    case VK_F6:         return LDL::Enums::KeyboardKey::F6;
+    case VK_F7:         return LDL::Enums::KeyboardKey::F7;
+    case VK_F8:         return LDL::Enums::KeyboardKey::F8;
+    case VK_F9:         return LDL::Enums::KeyboardKey::F9;
+    case VK_F10:        return LDL::Enums::KeyboardKey::F10;
+    case VK_F11:        return LDL::Enums::KeyboardKey::F11;
+    case VK_F12:        return LDL::Enums::KeyboardKey::F12;
+    case VK_F13:        return LDL::Enums::KeyboardKey::F13;
+    case VK_F14:        return LDL::Enums::KeyboardKey::F14;
+    case VK_F15:        return LDL::Enums::KeyboardKey::F15;
+    case VK_LEFT:       return LDL::Enums::KeyboardKey::Left;
+    case VK_RIGHT:      return LDL::Enums::KeyboardKey::Right;
+    case VK_UP:         return LDL::Enums::KeyboardKey::Up;
+    case VK_DOWN:       return LDL::Enums::KeyboardKey::Down;
+    case VK_NUMPAD0:    return LDL::Enums::KeyboardKey::Numpad0;
+    case VK_NUMPAD1:    return LDL::Enums::KeyboardKey::Numpad1;
+    case VK_NUMPAD2:    return LDL::Enums::KeyboardKey::Numpad2;
+    case VK_NUMPAD3:    return LDL::Enums::KeyboardKey::Numpad3;
+    case VK_NUMPAD4:    return LDL::Enums::KeyboardKey::Numpad4;
+    case VK_NUMPAD5:    return LDL::Enums::KeyboardKey::Numpad5;
+    case VK_NUMPAD6:    return LDL::Enums::KeyboardKey::Numpad6;
+    case VK_NUMPAD7:    return LDL::Enums::KeyboardKey::Numpad7;
+    case VK_NUMPAD8:    return LDL::Enums::KeyboardKey::Numpad8;
+    case VK_NUMPAD9:    return LDL::Enums::KeyboardKey::Numpad9;
+    case 'A':           return LDL::Enums::KeyboardKey::A;
+    case 'Z':           return LDL::Enums::KeyboardKey::Z;
+    case 'E':           return LDL::Enums::KeyboardKey::E;
+    case 'R':           return LDL::Enums::KeyboardKey::R;
+    case 'T':           return LDL::Enums::KeyboardKey::T;
+    case 'Y':           return LDL::Enums::KeyboardKey::Y;
+    case 'U':           return LDL::Enums::KeyboardKey::U;
+    case 'I':           return LDL::Enums::KeyboardKey::I;
+    case 'O':           return LDL::Enums::KeyboardKey::O;
+    case 'P':           return LDL::Enums::KeyboardKey::P;
+    case 'Q':           return LDL::Enums::KeyboardKey::Q;
+    case 'S':           return LDL::Enums::KeyboardKey::S;
+    case 'D':           return LDL::Enums::KeyboardKey::D;
+    case 'F':           return LDL::Enums::KeyboardKey::F;
+    case 'G':           return LDL::Enums::KeyboardKey::G;
+    case 'H':           return LDL::Enums::KeyboardKey::H;
+    case 'J':           return LDL::Enums::KeyboardKey::J;
+    case 'K':           return LDL::Enums::KeyboardKey::K;
+    case 'L':           return LDL::Enums::KeyboardKey::L;
+    case 'M':           return LDL::Enums::KeyboardKey::M;
+    case 'W':           return LDL::Enums::KeyboardKey::W;
+    case 'X':           return LDL::Enums::KeyboardKey::X;
+    case 'C':           return LDL::Enums::KeyboardKey::C;
+    case 'V':           return LDL::Enums::KeyboardKey::V;
+    case 'B':           return LDL::Enums::KeyboardKey::B;
+    case 'N':           return LDL::Enums::KeyboardKey::N;
+    case '0':           return LDL::Enums::KeyboardKey::Num0;
+    case '1':           return LDL::Enums::KeyboardKey::Num1;
+    case '2':           return LDL::Enums::KeyboardKey::Num2;
+    case '3':           return LDL::Enums::KeyboardKey::Num3;
+    case '4':           return LDL::Enums::KeyboardKey::Num4;
+    case '5':           return LDL::Enums::KeyboardKey::Num5;
+    case '6':           return LDL::Enums::KeyboardKey::Num6;
+    case '7':           return LDL::Enums::KeyboardKey::Num7;
+    case '8':           return LDL::Enums::KeyboardKey::Num8;
+    case '9':           return LDL::Enums::KeyboardKey::Num9;
     }
 
-    return result;
+    return LDL::Enums::KeyboardKey::Unknown;
 }
 
 LRESULT CALLBACK MainWindow::Handler(UINT Message, WPARAM WParam, LPARAM LParam)
