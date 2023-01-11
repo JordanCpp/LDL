e4ce60c322508a948f742dcf3b2e210629032bcb
Евгений <jordan631@yandex.ru>
2023-01-11 09:53:08 +0400

    Update test Surface

--- test/Graphics/Surface.cpp
+++ test/Graphics/Surface.cpp
@@ -65,6 +65,27 @@ void Clear()
 	LDL_TEST_EQUAL(surface.Empty() == true);
 }
 
+void ClearCapacity()
+{
+	Surface surface(Size, 4, Capacity);
+
+	surface.Clear();
+
+	LDL_TEST_EQUAL(surface.Pixels() != nullptr);
+
+	LDL_TEST_EQUAL(surface.Capacity().PosX() == Capacity.PosX());
+	LDL_TEST_EQUAL(surface.Capacity().PosY() == Capacity.PosY());
+
+	LDL_TEST_EQUAL(surface.Size().PosX() == 0);
+	LDL_TEST_EQUAL(surface.Size().PosY() == 0);
+
+	LDL_TEST_EQUAL(surface.Bytes() == Capacity.PosX() * Capacity.PosY() * 4);
+
+	LDL_TEST_EQUAL(surface.BytesPerPixel() == 4);
+
+	LDL_TEST_EQUAL(surface.Empty() == true);
+}
+
 void ClearColor()
 {
 	Surface surface(Size, 4);
@@ -96,12 +117,45 @@ void ClearColor()
 	}
 }
 
+void ClearColorCapacity()
+{
+	Surface surface(Size, 4, Capacity);
+
+	surface.Clear(Color(1, 2, 3, 4));
+
+	LDL_TEST_EQUAL(surface.Pixels() != nullptr);
+
+	LDL_TEST_EQUAL(surface.Capacity().PosX() == Capacity.PosX());
+	LDL_TEST_EQUAL(surface.Capacity().PosY() == Capacity.PosY());
+
+	LDL_TEST_EQUAL(surface.Size().PosX() == Size.PosX());
+	LDL_TEST_EQUAL(surface.Size().PosY() == Size.PosY());
+
+	LDL_TEST_EQUAL(surface.Bytes() == Capacity.PosX() * Capacity.PosY() * 4);
+
+	LDL_TEST_EQUAL(surface.BytesPerPixel() == 4);
+
+	LDL_TEST_EQUAL(surface.Empty() == false);
+
+	Color* pixels = (Color*)surface.Pixels();
+
+	for (size_t i = 0; i < surface.Size().PosX() * surface.Size().PosY(); i++)
+	{
+		LDL_TEST_EQUAL(pixels[i].Red() == 1);
+		LDL_TEST_EQUAL(pixels[i].Green() == 2);
+		LDL_TEST_EQUAL(pixels[i].Blue() == 3);
+		LDL_TEST_EQUAL(pixels[i].Alpha() == 4);
+	}
+}
+
 int main()
 {
 	Init();
 	InitCapacity();
 	Clear();
+	ClearCapacity();
 	ClearColor();
+	ClearColorCapacity();
 
 	return 0;
 }
