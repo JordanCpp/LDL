a005cd7fec37442d81608edc436b2844815be205
Евгений <jordan631@yandex.ru>
2023-01-12 14:52:00 +0400

    Fix Win9x build

--- Win9x/MinGW_Build.bat
+++ Win9x/MinGW_Build.bat
@@ -1,22 +1,21 @@
 call "C:\Downloads\codeblocks-12.11mingw-setup\MinGW\mingwvars.bat"
 
 cd ../
-g++ -Wall -Wextra -pedantic -O2 -std=c++98 -o Win9x\%1 -DLDL_GPU_SUPPORT_OPENGL1 %2 ^
+g++ -Wall -Wextra -pedantic -O2 -std=c++98 -o Win9x\%1 -DLDL_RENDER_OPENGL1 %2 ^
 source\Allocators\*.cpp ^
 source\Core\*.cpp ^
 source\Events\*.cpp ^
 source\Time\*.cpp ^
 source\Platforms\Windows\Graphics\MainWindow.cpp ^
 source\Platforms\Windows\Graphics\Cpu\CpuWindowImpl.cpp ^
-source\Platforms\Windows\Graphics\GL1\*.cpp ^
+source\Platforms\Windows\Graphics\OpenGL1\*.cpp ^
 source\Platforms\Windows\Time\Ticks.cpp ^
 source\Platforms\Windows\Core\*.cpp ^
 source\Platforms\Windows\Input\*.cpp ^
 source\Graphics\*.cpp ^
 source\Graphics\Base\*.cpp ^
 source\Graphics\Cpu\*.cpp ^
-source\Graphics\GL1\*.cpp ^
-source\Graphics\Gpu\*.cpp ^
+source\Graphics\OpenGL1\*.cpp ^
 source\Graphics\Primitives\*.cpp ^
 source\Loaders\*.cpp ^
 source\OpenGL\*.cpp ^
--- Win9x/VC6_Build.bat
+++ Win9x/VC6_Build.bat
@@ -1,21 +1,20 @@
 
 call "C:\VC6\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
 
-CL -GX /I..\include /DLDL_GPU_SUPPORT_OPENGL1 ..\%2  ^
+CL -GX /I..\include /DLDL_RENDER_OPENGL1 ..\%2  ^
 ..\source\Allocators\*.cpp ^
 ..\source\Core\*.cpp ^
 ..\source\Events\*.cpp ^
 ..\source\Time\*.cpp ^
 ..\source\Platforms\Windows\Graphics\MainWindow.cpp ^
 ..\source\Platforms\Windows\Graphics\Cpu\CpuWindowImpl.cpp ^
-..\source\Platforms\Windows\Graphics\Gl1\*.cpp ^
+..\source\Platforms\Windows\Graphics\OpenGL1\*.cpp ^
 ..\source\Platforms\Windows\Time\Ticks.cpp ^
 ..\source\Platforms\Windows\Core\*.cpp ^
 ..\source\Graphics\*.cpp ^
 ..\source\Graphics\Base\*.cpp ^
 ..\source\Graphics\Cpu\*.cpp ^
-..\source\Graphics\GL1\*.cpp ^
-..\source\Graphics\Gpu\*.cpp ^
+..\source\Graphics\OpenGL1\*.cpp ^
 ..\source\Graphics\Primitives\*.cpp ^
 ..\source\Loaders\*.cpp  ^
 ..\source\Platforms\Windows\Input\*.cpp  ^
