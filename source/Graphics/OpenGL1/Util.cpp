029b0b07e8ad11c5025788c798414c675415bb0e
Евгений <jordan631@yandex.ru>
2023-01-16 00:40:20 +0400

    Deleted unused code

--- source/Graphics/OpenGL1/Util.cpp
+++ source/Graphics/OpenGL1/Util.cpp
@@ -67,32 +67,6 @@ void GpuUtil::DrawQuad(const Point2u& dstPos, const Point2u& dstSize, const Poin
 	glEnd();
 }
 
-void GpuUtil::DrawQuad(const Point2u& dstPos, const Point2u& dstSize, const Point2u& srcPos, const Point2u& srcSize, const Point2u& size, size_t textureSize)
-{
-	GLfloat x = (GLfloat)dstPos.PosX();
-	GLfloat y = (GLfloat)dstPos.PosY();
-	GLfloat w = (GLfloat)dstSize.PosX();
-	GLfloat h = (GLfloat)dstSize.PosY();
-
-	GLfloat cx = (GLfloat)srcPos.PosX();
-	GLfloat cy = (GLfloat)srcPos.PosY();
-	GLfloat cw = (GLfloat)srcSize.PosX();
-	GLfloat ch = (GLfloat)srcSize.PosY();
-
-	GLfloat dcx = cx / w;
-	GLfloat dcy = cy / h;
-
-	GLfloat dcw = (cx + cw) / w;
-	GLfloat dch = (cy + ch) / h;
-
-	glBegin(GL_QUADS);
-	glTexCoord2f(dcx, dcy); glVertex2f(x, y);
-	glTexCoord2f(dcw, dcy); glVertex2f(x + cw, y);
-	glTexCoord2f(dcw, dch); glVertex2f(x + cw, y + ch);
-	glTexCoord2f(dcx, dch); glVertex2f(x, y + ch);
-	glEnd();
-}
-
 void GpuUtil::Normalize(const Color& color, GLclampf& r, GLclampf& g, GLclampf& b)
 {
 	 r = color.Red() / 255.0f;
