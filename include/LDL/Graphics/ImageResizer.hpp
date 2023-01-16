e96ecde84cfa88d02b4a1a64522e03ff7438d639
Евгений <jordan631@yandex.ru>
2023-01-16 11:06:32 +0400

    Add stb_image_resize

---
+++ include/LDL/Graphics/ImageResizer.hpp
@@ -0,0 +1,18 @@
+#ifndef LDL_Graphics_ImageResizer_hpp
+#define LDL_Graphics_ImageResizer_hpp
+
+#include <LDL/Graphics/Primitives/Point2u.hpp>
+
+namespace LDL
+{
+	namespace Graphics
+	{
+		class ImageResizer
+		{
+		public:
+		private:
+		};
+	}
+}
+
+#endif    
---
+++ source/Graphics/ImageResizer.cpp
@@ -0,0 +1,4 @@
+#include <LDL/Graphics/ImageResizer.hpp>
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "../../dependencies/stb/stb_image_resize.h"
