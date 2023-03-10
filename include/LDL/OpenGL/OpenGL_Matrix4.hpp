cb15bacb9220553e90f501e894c4683d60f6b338
Евгений <jordan631@yandex.ru>
2023-03-10 10:28:07 +0400

    Update Matrix4

--- include/LDL/OpenGL/OpenGL_Matrix4.hpp
+++ include/LDL/OpenGL/OpenGL_Matrix4.hpp
@@ -13,9 +13,17 @@ namespace LDL
 			Max = 16
 		};
 		Matrix4();
+		Matrix4& operator= (const Matrix4& source);
 		GLdouble* Values();
 		void Identity();
 		void Perspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar);
+		Matrix4 operator * (const Matrix4& m) const;
+		void Translate(GLdouble x, GLdouble y, GLdouble z);
+		void RotateX(GLdouble radians);
+		void RotateY(GLdouble radians);
+		void RotateZ(GLdouble radians);
+		void Scale(GLdouble sx, GLdouble sy, GLdouble sz);
+		void Ortho(GLdouble left, GLdouble right, GLdouble bottom, GLdouble top, GLdouble farv, GLdouble nearv);
 	private:
 		GLdouble _Values[Max];
 	};
