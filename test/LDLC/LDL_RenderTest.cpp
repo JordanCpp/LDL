69ac8dd82f72f6f0c630f0c1fe17a09c43d09fe2
Евгений <jordan631@yandex.ru>
2023-04-01 00:09:41 +0400

    Add test for LDLC

---
+++ test/LDLC/LDL_RenderTest.cpp
@@ -0,0 +1,25 @@
+#include <LDL/Core/TestEqual.hpp>
+#include <LDLC/LDL_Render.h>
+
+int main()
+{
+	LDL_Window* window = LDL_WindowNew(0, 0, 640, 480, "LDL_WindowTest", 1);
+
+	LDL_RenderContext* renderContext = LDL_RenderContextNew();
+	LDL_TEST_EQUAL(renderContext != NULL);
+
+	LDL_Render* render = LDL_RenderNew(renderContext, window);
+	LDL_TEST_EQUAL(render != NULL);
+
+	LDL_TEST_EQUAL(LDL_RenderGetSizeX(render) > 0);
+	LDL_TEST_EQUAL(LDL_RenderGetSizeX(render) <= 640);
+
+	LDL_TEST_EQUAL(LDL_RenderGetSizeY(render) > 0);
+	LDL_TEST_EQUAL(LDL_RenderGetSizeY(render) <= 480);
+
+	LDL_RenderContextFree(renderContext);
+	LDL_RenderFree(render);
+	LDL_WindowFree(window);
+
+	return 0;
+}
--- test/LDLC/LDL_WindowTest.cpp
+++ test/LDLC/LDL_WindowTest.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-	LDL_Window* window = LDL_WindowNew(5, 10, 800, 600, "LDL_WindowTest", 1);
+	LDL_Window* window = LDL_WindowNew(5, 10, 640, 480, "LDL_WindowTest", 1);
 
 	LDL_TEST_EQUAL(window != NULL);
 	LDL_TEST_EQUAL(LDL_WindowGetPosX(window)  == 5);
 	LDL_TEST_EQUAL(LDL_WindowGetPosY(window)  == 10);
-	LDL_TEST_EQUAL(LDL_WindowGetSizeX(window) == 800);
-	LDL_TEST_EQUAL(LDL_WindowGetSizeY(window) == 600);
+	LDL_TEST_EQUAL(LDL_WindowGetSizeX(window) == 640);
+	LDL_TEST_EQUAL(LDL_WindowGetSizeY(window) == 480);
 	LDL_TEST_EQUAL(strcmp(LDL_WindowGetTitle(window), "LDL_WindowTest") == 0);
 
 	LDL_WindowSetTitle(window, "New LDL_WindowTest");
