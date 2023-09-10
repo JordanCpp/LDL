c23a48a4738f9491ea6e3a7a457077387c5ebd41
Evgeniy <61433942+JordanCpp@users.noreply.github.com>
2023-09-10 19:16:57 +0400

    Add examples

---
+++ examples/cpp/cpp01.cpp
@@ -0,0 +1,51 @@
+#include <LDL/Cpp/Render.hpp>
+#include <LDL/Low/FpsCount.hpp>
+#include <LDL/Low/Conv.hpp>
+#include <stdio.h>
+
+int main()
+{
+	LDL_ErrorHandler errorHandler;
+	LDL_RenderContext renderContext;
+
+	LDL_Window window(&errorHandler, &renderContext, LDL_Point2u(0, 0), LDL_Point2u(800, 600), "", LDL_WindowMode::Fixed);
+
+	if (errorHandler.Ok())
+	{
+		LDL_Render render(&renderContext, &window);
+
+		LDL_Event report;
+
+		LDL_FpsCounter fpsCounter;
+		LDL_NumberToString convert;
+
+		while (window.Running())
+		{
+			fpsCounter.Start();
+
+			while (window.GetEvent(report))
+			{
+				if (report.Type == LDL_Events::IsQuit)
+				{
+					window.StopEvent();
+				}
+			}
+
+			render.End();
+
+			if (fpsCounter.Calc())
+			{
+				window.Title(convert.Convert(fpsCounter.Fps()));
+				fpsCounter.Clear();
+			}
+
+			window.PollEvents();
+		}
+	}
+	else
+	{
+		printf("%s\n", errorHandler.Message());
+	}
+
+	return 0;
+}
