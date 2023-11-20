b371ec185d725d7bb138ee99d839adbddec11a06
Evgeniy <61433942+JordanCpp@users.noreply.github.com>
2023-11-20 09:02:14 +0400

    Refactoring color

--- include/LDL/Graphics/Color.hpp
+++ include/LDL/Graphics/Color.hpp
@@ -1,19 +1,47 @@
 #ifndef LDL_Graphics_Color_hpp
 #define LDL_Graphics_Color_hpp
 
-#include <LDL/Core/Types.hpp>
 #include <LDL/Config.hpp>
 
 namespace LDL
 {
 	namespace Graphics
 	{
-		class LDL_LIBRARY Color
+		class Color
 		{
 		public:
-			Color();
-			Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha = 255);
-			uint32_t toInt() const;
+			Color::Color() :
+#if defined(LDL_CONFIG_COLOR_BGRA)
+				b(0),
+				g(0),
+				r(0),
+#else
+				r(0),
+				g(0),
+				b(0),
+#endif
+				a(255)
+			{
+			}
+
+			Color::Color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha = 255) :
+#if defined(LDL_CONFIG_COLOR_BGRA)
+				b(blue),
+				g(green),
+				r(red),
+#else
+				r(red),
+				g(green),
+				b(blue),
+#endif
+				a(alpha)
+			{
+			}
+
+			uint32_t Color::toInt() const
+			{
+				return (uint32_t)((r << 24) | (g << 16) | (b << 8) | a);
+			}
 
         #if defined(LDL_CONFIG_COLOR_BGRA)
 			uint8_t b;
