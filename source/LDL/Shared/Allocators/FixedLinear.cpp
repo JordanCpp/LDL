20567f291d1a5abb93da551b51f9339508440c2c
JordanCpp <Jordan631@yandex.ru>
2025-08-24 01:04:32 +0400

    Fix FixedLinear

--- source/LDL/Shared/Allocators/FixedLinear.cpp
+++ source/LDL/Shared/Allocators/FixedLinear.cpp
@@ -4,46 +4,72 @@
 // https://www.boost.org/LICENSE_1_0.txt)
 
 #include <LDL/Allocators/FixedLinear.hpp>
-#include <assert.h>
 
 using namespace LDL::Allocators;
 
-FixedLinear::FixedLinear(size_t bytes, Allocator* allocator):
+FixedLinear::FixedLinear(size_t bytes, Allocator* allocator) :
 	_capacity(bytes),
 	_position(0),
 	_content(NULL),
 	_allocator(allocator)
 {
 	if (_allocator)
+	{
 		_content = (uint8_t*)_allocator->Allocate(_capacity);
+	}
 	else
+	{
 		_content = new uint8_t[_capacity];
+	}
 }
 
 FixedLinear::~FixedLinear()
 {
 	if (_allocator)
+	{
 		_allocator->Deallocate(_content);
+	}
 	else
+	{
 		delete[] _content;
+	}
 }
 
 void* FixedLinear::Allocate(size_t bytes)
 {
-	assert(bytes > 0);
+	if (bytes == 0)
+	{
+		return NULL;
+	}
 
-	assert(_position + bytes <= _capacity);
+	if (_position + bytes > _capacity)
+	{
+		return NULL;
+	}
 
 	void* result = _content + _position;
-
 	_position += bytes;
-
 	return result;
 }
 
 void* FixedLinear::AllocateAligned(size_t bytes, size_t alignment)
 {
-	return NULL;
+	size_t current = (size_t)(_content + _position);
+	size_t aligned = (current + alignment - 1) & ~(alignment - 1);
+	size_t padding = aligned - current;
+
+	if (_position + padding + bytes > _capacity)
+	{
+		return NULL;
+	}
+
+	_position += padding;
+
+	void* result = _content + _position;
+	
+	_position += bytes;
+
+	return result;
 }
 
 void FixedLinear::Deallocate(void* ptr)
