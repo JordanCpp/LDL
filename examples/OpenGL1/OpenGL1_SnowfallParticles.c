ba6462eec7ae6ae80d1712fb5dee8a81648455c7
ZoschukEN <ZoschukEN@RTK-CD.RU>
2026-04-30 18:15:36 +0400

    Fix examples license Public Domain (CC0 1.0 Universal)

--- examples/OpenGL1/OpenGL1_SnowfallParticles.c
+++ examples/OpenGL1/OpenGL1_SnowfallParticles.c
@@ -1,16 +1,9 @@
 /*
-Copyright(C) 2026 Evgeny Zoshchuk (JordanCpp)
-
-This library is free software; you can redistribute it and/or modify it
-under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This library is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
-License for more details.
-*/
+ * -----------------------------------------------------------------------------
+ * This example is in the public domain (CC0 1.0 Universal).
+ * You can copy, modify, use, and distribute it for any purpose.
+ * -----------------------------------------------------------------------------
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
