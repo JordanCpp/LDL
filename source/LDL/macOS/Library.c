70ce71c2d24c5c6fe077690b5626270626b5a3d2
AslanD <122040070+Ztry8@users.noreply.github.com>
2026-05-21 16:11:51 +0500

    Add `Library.c` file

---
+++ source/LDL/macOS/Library.c
@@ -0,0 +1,101 @@
+/*
+Copyright(C) 2026 AslanD (Ztry8)
+
+This library is free software; you can redistribute it and /or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.See the GNU Lesser General Public
+License for more details.
+*/
+
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <LDL/Library.h>
+
+typedef struct LDL_Library
+{
+    LDL_Result* Result;
+    void*       Library;
+} LDL_Library;
+
+LDL_Library* LDL_LibraryNew(LDL_Result* result)
+{
+    LDL_Library* library = (LDL_Library*)malloc(sizeof(LDL_Library));
+
+    if (library)
+    {
+        library->Library = NULL;
+        library->Result  = result;
+
+        return library;
+    }
+
+    return NULL;
+}
+
+void LDL_LibraryFree(LDL_Library* library)
+{
+    if (library)
+    {
+        if (library->Library != NULL)
+        {
+            dlclose(library->Library);
+        }
+
+        free(library);
+    }
+}
+
+bool LDL_LibraryOpen(LDL_Library* library, const char* path)
+{
+    if (library)
+    {
+        LDL_LibraryClose(library);
+
+        library->Library = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
+
+        if (library->Library == NULL && library->Result)
+        {
+            LDL_ResultAddMessage(library->Result, "Load library failed: %s\n", path);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+void LDL_LibraryClose(LDL_Library* library)
+{
+    if (library && library->Library != NULL)
+    {
+        dlclose(library->Library);
+        library->Library = NULL;
+    }
+}
+
+LDL_VoidFuncPtr LDL_LibraryGetFunction(LDL_Library* library, const char* name)
+{
+    LDL_VoidFuncPtr result = NULL;
+
+    if (library)
+    {
+        if (library->Library)
+        {
+            result = (LDL_VoidFuncPtr)dlsym(library->Library, name);
+
+            if (result == NULL && library->Result)
+            {
+                LDL_ResultAddMessage(library->Result, "GetProcAddress failed: %s\n", name);
+            }
+        }
+    }
+
+    return result;
+}
