0b5c5932327f0965ee3f589914a848488585a25c
AslanD <122040070+Ztry8@users.noreply.github.com>
2026-06-22 14:45:12 +0500

    Add Emscripten/WebAssembly backend for web platform support

---
+++ cmake/emscripten/EmscriptenTarget.cmake
@@ -0,0 +1,60 @@
+function(LDL_AddEmscriptenTarget)
+    cmake_parse_arguments(
+        ARG                        
+        "ASYNCIFY"                 
+        "TARGET;SHELL"             
+        "SOURCES;LINK_FLAGS"      
+        ${ARGN}
+    )
+
+    if(NOT ARG_TARGET)
+        message(FATAL_ERROR "LDL_AddEmscriptenTarget: TARGET is required")
+    endif()
+
+    if(NOT ARG_SOURCES)
+        message(FATAL_ERROR "LDL_AddEmscriptenTarget: SOURCES is required")
+    endif()
+
+    if(NOT ARG_SHELL)
+        set(ARG_SHELL "${CMAKE_CURRENT_LIST_DIR}/shell.html")
+    endif()
+
+    add_executable(${ARG_TARGET} ${ARG_SOURCES})
+
+    set_target_properties(${ARG_TARGET} PROPERTIES SUFFIX ".html")
+
+    target_link_libraries(${ARG_TARGET} PRIVATE LDL::Library)
+
+    set(_EM_FLAGS
+        # Use our custom HTML shell
+        "--shell-file" "${ARG_SHELL}"
+        # WebGL 1 + 2
+        "-sUSE_WEBGL2=1"
+        "-sFULL_ES3=1"
+        "-sMIN_WEBGL_VERSION=1"
+        "-sMAX_WEBGL_VERSION=2"
+        # Memory
+        "-sALLOW_MEMORY_GROWTH=1"
+        # Export the main() entry point
+        "-sEXPORTED_FUNCTIONS=[\"_main\"]"
+        "-sEXPORTED_RUNTIME_METHODS=[\"ccall\",\"cwrap\"]"
+        # Keep alive for requestAnimationFrame
+        "-sNO_EXIT_RUNTIME=1"
+        # Runtime assertions: 2 = verbose, shows function signatures in indirect call errors
+        # Set to 0 in final production builds to reduce output size
+        "-sASSERTIONS=2"
+    )
+
+    if(ARG_ASYNCIFY)
+        list(APPEND _EM_FLAGS "-sASYNCIFY=1")
+        target_compile_definitions(${ARG_TARGET} PRIVATE ASYNCIFY=1)
+    endif()
+
+    if(ARG_LINK_FLAGS)
+        list(APPEND _EM_FLAGS ${ARG_LINK_FLAGS})
+    endif()
+
+    target_link_options(${ARG_TARGET} PRIVATE ${_EM_FLAGS})
+
+    message(STATUS "LDL Emscripten target: ${ARG_TARGET}")
+endfunction()
---
+++ source/LDL/Emscripten/Window.c
@@ -0,0 +1,187 @@
+/*
+Copyright(C) 2026 AslanD (Ztry8)
+
+This library is free software; you can redistribute it and /or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.See the GNU Lesser General Public
+License for more details.
+*/
+
+#include <stdlib.h>
+#include <LDL/Window.h>
+#include <LDL/Emscripten/WinGL.h>
+
+struct LDL_Window
+{
+	LDL_Result*      Result;
+	LDL_Context*     Context;
+	LDL_WindowOpenGL WindowOpenGL;
+};
+
+LDL_Window* LDL_WindowCreate(LDL_Result* result, LDL_Context* context, LDL_Vec2i pos, LDL_Vec2i size, const char* title, size_t mode)
+{
+	LDL_Window* window = (LDL_Window*)malloc(sizeof(LDL_Window));
+
+	if (window && result && context)
+	{
+		window->Context = context;
+		window->Result  = result;
+
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			LDL_WindowOpenGLInit(&window->WindowOpenGL, result, pos, size, title, mode);
+			break;
+		};
+
+		return window;
+	}
+
+	if (window)
+	{
+		free(window);
+	}
+
+	return NULL;
+}
+
+void LDL_WindowDestroy(LDL_Window* window)
+{
+	if (window)
+	{
+		if (window->Context)
+		{
+			switch (LDL_ContextGet(window->Context))
+			{
+			case LDL_ContextOpenGLLegacy:
+			case LDL_ContextOpenGLHybrid:
+			case LDL_ContextOpenGLModern:
+				LDL_WindowOpenGLDeinit(&window->WindowOpenGL);
+				break;
+			};
+		}
+
+		free(window);
+	}
+}
+
+bool LDL_WindowIsRunning(LDL_Window* window)
+{
+	if (window && window->Context)
+	{
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			return LDL_WindowOpenGLIsRunning(&window->WindowOpenGL);
+		};
+	}
+
+	return false;
+}
+
+void LDL_WindowStopEvent(LDL_Window* window)
+{
+	if (window && window->Context)
+	{
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			LDL_WindowOpenGLStopEvent(&window->WindowOpenGL);
+			break;
+		};
+	}
+}
+
+bool LDL_WindowGetEvent(LDL_Window* window, LDL_Event* event)
+{
+	if (window && window->Context && event)
+	{
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			return LDL_WindowOpenGLGetEvent(&window->WindowOpenGL, event);
+		};
+	}
+
+	return false;
+}
+
+LDL_Vec2i LDL_WindowGetSize(LDL_Window* window)
+{
+	if (window && window->Context)
+	{
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			return LDL_WindowOpenGLGetSize(&window->WindowOpenGL);
+		};
+	}
+
+	return LDL_GetVec2i(0, 0);
+}
+
+const char* LDL_WindowGetTitle(LDL_Window* window)
+{
+	if (window && window->Context)
+	{
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			return LDL_WindowOpenGLGetTitle(&window->WindowOpenGL);
+		};
+	}
+
+	return NULL;
+}
+
+void LDL_WindowSetTitle(LDL_Window* window, const char* title)
+{
+	if (window && window->Context)
+	{
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			LDL_WindowOpenGLSetTitle(&window->WindowOpenGL, title);
+			break;
+		};
+	}
+}
+
+void LDL_WindowPresent(LDL_Window* window)
+{
+	if (window && window->Context)
+	{
+		switch (LDL_ContextGet(window->Context))
+		{
+		case LDL_ContextOpenGLLegacy:
+		case LDL_ContextOpenGLHybrid:
+		case LDL_ContextOpenGLModern:
+			LDL_WindowOpenGLPresent(&window->WindowOpenGL);
+			break;
+		};
+	}
+}
+
+LDL_Surface* LDL_WindowGetScreen(LDL_Window* window)
+{
+	return NULL;
+}
